f8cf5ff88b66ff7311e347e0b2264954dea7afb2
Allyson Barros <allysonbarrosrn@gmail.com>
2024-12-27 11:35:41 -0300

    Subindo o código para o Github.

--- app.py
+++ app.py
@@ -7,25 +7,22 @@ client = Groq(
 )
 
 # Initialize the Groq pipeline
-def generate_message(recruiter_name, job_description):
-    prompt = f"Escreva uma mensagem em inglês de conexão com obrigatoriamente até 200 carecteres para o recrutador {recruiter_name} sobre a vaga com a seguinte descrição: {job_description}"
-
+def generate_message(my_experiences, recruiter_name, job_description):
     chat_completion = client.chat.completions.create(
         messages=[
             {
                 "role": "system",
-                "content": """Você é um assistente responsável por gerar mensagens curtas de conexão no LinkedIn para recrutadores que estão em busca de um talentoso profissional de TI. 
+                "content": f"""Você é um assistente responsável por gerar mensagens curtas de conexão no LinkedIn para recrutadores que estão em busca de um talentoso profissional de TI. 
                 O objetivo é chamar a atenção do recrutador e despertar o interesse dele em mim como candidato. 
                 IMPORTANTE: Você deve retornar APENAS a mensagem. A mensagem deve ser personalizada e direcionada ao recrutador, e não ao candidato. 
                 IMPORTANTE: A mensagem deve ser em inglês e ter no máximo 200 caracteres.
 
-                Esse é o meu resumo profissional: Possuo 15 anos de experiência em desenvolvimento de software, com foco em aplicações web. 
-                Sou especialista em Python, Django e Engenharia de Dados. Tenho experiência em liderança de equipes e projetos. 
-                Sou apaixonado por tecnologia e estou sempre em busca de novos desafios.""",
+                Esse é o meu resumo profissional: {my_experiences}
+                """,
             },
             {
                 "role": "user",
-                "content": prompt,
+                "content": f"Escreva uma mensagem em inglês de conexão com obrigatoriamente até 200 carecteres para o recrutador {recruiter_name} sobre a vaga com a seguinte descrição: {job_description}",
             }
         ],
         model="llama3-8b-8192",
@@ -39,12 +36,16 @@ st.write("O objetivo é chamar a atenção do recrutador e despertar o interesse
 
 st.subheader("Insira os detalhes da vaga e do recrutador")
 
+my_experiences = st.text_area("Resumo Profissional", height=200)
+
+st.subheader("Insira os detalhes da vaga e do recrutador")
+
 recruiter_name = st.text_input("Nome do Recrutador")
-job_description = st.text_area("Descrição da Vaga")
+job_description = st.text_area("Descrição da Vaga", height=200)
 
 if st.button("Gerar Mensagem"):
-    if recruiter_name and job_description:
-        message = generate_message(recruiter_name, job_description)
+    if my_experiences and recruiter_name and job_description:
+        message = generate_message(my_experiences, recruiter_name, job_description)
 
         # Display the generated message and the size of the message
         st.text_area("Mensagem Gerada", value=message, height=200)
